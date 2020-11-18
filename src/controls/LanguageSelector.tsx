f1787659a8854247b922d5e19cb798189eb6b0e5
sergeiwork <sergeiwork@yahoo.com>
2020-11-18 16:06:58 +0300

    Updated Language selector and filled manifest Basic sceleton to show before react loaded

--- src/controls/LanguageSelector.tsx
+++ src/controls/LanguageSelector.tsx
@@ -1,23 +1,33 @@
 import React, { Component } from "react";
-import { LocalizeContextProps, withLocalize } from "react-localize-redux";
+import {
+  LocalizeContextProps,
+  Translate,
+  withLocalize,
+} from "react-localize-redux";
 import { Select } from "react-materialize";
 
 class LanguageSelector extends Component<LocalizeContextProps> {
   render() {
     return (
-      <Select
-        onChange={(e) => {
-          this.props.setActiveLanguage(e.target.value);
-          localStorage.setItem("language", e.target.value);
-        }}
-        value={this.props.activeLanguage && this.props.activeLanguage.code}
-      >
-        {this.props.languages?.map((lng) => (
-          <option value={lng.code} key={lng.code}>
-            {lng.name}
-          </option>
-        ))}
-      </Select>
+      <>
+        <label htmlFor="languageSelector">
+          <Translate id="selectLanguageLabel" />
+        </label>
+        <Select
+          onChange={(e) => {
+            this.props.setActiveLanguage(e.target.value);
+            localStorage.setItem("language", e.target.value);
+          }}
+          value={this.props.activeLanguage && this.props.activeLanguage.code}
+          id="languageSelector"
+        >
+          {this.props.languages?.map((lng) => (
+            <option value={lng.code} key={lng.code}>
+              {lng.name}
+            </option>
+          ))}
+        </Select>
+      </>
     );
   }
 }
